7c254a008cb44a8ca5c014b231b6fcbfdd540e57
Juanjoo-Alvarez <riv24856@uvg.edu.gt>
2025-03-26 11:09:29 -0600

    Agregando UML

--- Proyecto1/target/classes/codigo.lisp
+++ Proyecto1/target/classes/codigo.lisp
@@ -1,6 +1,10 @@
-(defun mcd (a b)
-  (cond
-    ((= b 0) a)
-    (t (mcd b (mod a b)))))
-(print (mcd 56 98))
-(atom 3)
+(setq a 10)
+(setq b 6)
+(setq c 5)
+(cond
+((>= a b)
+(cond
+((>= a c) (print a))
+(t (print c))))
+((>= b c) (print b))
+(t (print c)))
